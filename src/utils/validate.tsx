58bf527337ace6cc341518147fdeaf6ff408b9c6
Matheus Alves <matalves12345@gmail.com>
2023-05-02 15:52:00 -0300

    feat: criação do componente de registro(sem validação)

---
+++ src/utils/validate.tsx
@@ -0,0 +1,3 @@
+export const validateEmail = new RegExp(
+    "/^[a-zA-Z0-9._%+-]*@[\w-]+ufcg\.edu\.br$/"
+)
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgrounds: {
+        'background': '#252525',
+        'backgroundButtonYellow': '#FFB400',
+        'backgroundInput': '#C2C2C2',
+        'backgroundContent': '#222222',
       },
+      fontFamily:{
+        'body':['"Poppins"']
+      }
     },
   },
   plugins: [],
