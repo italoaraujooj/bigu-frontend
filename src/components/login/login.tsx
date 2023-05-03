18cd514c3c0832cfd57e5d79e9c5568398bed3e7
Matheus Alves <matalves12345@gmail.com>
2023-05-03 13:40:16 -0300

    feat: criação do componente de login e geração de hash para senha no cadastro

---
+++ src/components/login/login.tsx
@@ -0,0 +1,57 @@
+import React, {useRef, useState} from "react";
+import { Form } from '@unform/web'
+import Input from '../input/input'
+import { SubmitHandler, FormHandles } from '@unform/core'
+import Image from "next/image";
+import Back from "../../assets/CaretRight.svg"
+import bcrypt from 'bcryptjs'
+
+interface UserLoginState {
+    email: string;
+    password: string;
+    
+}
+
+const handleSubmit: SubmitHandler<UserLoginState> = data => {
+    console.log(data.email)//verificar se o email está cadastrado no banco
+    
+    if(bcrypt.compareSync(encryptPassword(data.password), "$2a$10$0TWtAxZHow3epDrPJp1vfu6qXfijnnlwlQZsRGVD4S/IhYR7wUCa6")){//comparar com a senha cadastrada no banco))
+            //estaAutenticado
+    } 
+}
+    
+    function encryptPassword(password: string): string {
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+    return hash;
+}
+
+function Login(){
+    const formRef = useRef<FormHandles>(null);
+
+    return(
+        <div className="flex justify-center items-start h-screen fixed bg-white w-[100%] overflow-y-scroll pt-3 lg:right-0 lg:max-w-[30.125rem]">
+            <Form className="flex flex-col gap-5 justify-center"ref={formRef} onSubmit={handleSubmit}>
+                <Image className="w-10 h-10" src={Back} alt="voltar"/>
+                <h1 className="font-['Poppins'] font-semibold text-2xl md:text-4xl">
+                    Login
+                </h1>
+                <div className="flex flex-col gap-6">
+                    <div className="flex flex-col gap-3">
+                        <label className="font-['Poppins'] text-[#616161] text-sm font-bold md:text-lg">Email (Acadêmico)</label>
+                        <Input name="email" className="font-['Poppins'] placeholder-[#808080] block bg-[#C2C2C2] rounded-xl w-80 h-14 px-5 text-sm md:h-14 md:text-lg" type="email" placeholder="seu.nome@ufcg.edu.br" required/>
+                    </div>
+                    <div className="flex flex-col gap-3">
+                        <label className="font-['Poppins'] text-[#616161] text-sm font-bold md:text-lg">Senha:</label>
+                        <Input name="password" className="font-['Poppins'] placeholder-[#808080] block bg-[#C2C2C2] rounded-xl w-80 h-14 px-5 text-sm md:h-14 md:text-lg" type="password" placeholder="*********" required/>
+                    </div>
+                    <button className="bg-[#FFB400] rounded-lg h-12 font-bold text-white text-xl md:h-16 md:text-3xl" type="submit">Entrar</button>
+                </div>    
+            </Form>
+        </div>
+    )
+
+}
+
+export default Login;
--- src/components/register/register.tsx
+++ src/components/register/register.tsx
@@ -5,7 +5,7 @@ import { SubmitHandler, FormHandles } from '@unform/core'
 import Image from "next/image";
 import Back from "../../assets/CaretRight.svg"
 import { validateEmail, validateMatricula, validatePassword } from "@/utils/validate";
-
+import bcrypt from 'bcryptjs'
 
 interface UserFormState {
     name: string;
@@ -42,51 +42,59 @@ function Register(){
         setPasswordErr(false);
 
         if(!(emailErr && matriculaErr && passwordErr)){
-            //envia os dados para o Back
-            console.log(data)
+            const encryptedPassword = encryptPassword(data.password);
+            console.log(encryptedPassword)
+            //enviar dados para o backend aqui
         }
     }
 
+    function encryptPassword(password: string): string {
+        const saltRounds = 10;
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hash = bcrypt.hashSync(password, salt);
+        return hash;
+    }
+
     return(
-        <div className="flex justify-center items-start h-screen fixed bg-white w-[100%] overflow-y-scroll pt-3 md:right-0 md:max-w-[30.125rem]">
+        <div className="flex justify-center items-start h-screen fixed bg-white w-[100%] overflow-y-scroll pt-3 lg:right-0 lg:max-w-[30.125rem]">
                 <Form className="flex flex-col gap-5 justify-center"ref={formRef} onSubmit={handleSubmit}>
                     <Image className="w-10 h-10" src={Back} alt="voltar"/>
                     <h1 className="font-['Poppins'] font-semibold text-2xl md:text-4xl">
                         Criar Conta
                     </h1>
-                    <div className="flex flex-col gap-6">
-                        <div className="flex flex-col gap-3">
+                    <div className="flex flex-col gap-5">
+                        <div className="flex flex-col gap-2">
                             <label className="font-['Poppins'] text-[#616161] font-bold text-sm md:text-lg">Nome completo:</label>
-                            <Input name="name" className="font-['Poppins'] placeholder-[#808080] block bg-[#C2C2C2] rounded-xl w-80 h-14 px-5 text-sm md:h-20 md:text-lg" type = "text" placeholder="Exemplo eaosdlss" required />
+                            <Input name="name" className="font-['Poppins'] placeholder-[#808080] block bg-[#C2C2C2] rounded-xl w-80 h-14 px-5 text-sm md:h-14 md:text-base" type = "text" placeholder="Exemplo eaosdlss" required />
                         </div>
-                        <div className="flex flex-col gap-3">
+                        <div className="flex flex-col gap-2">
                             <label className="font-['Poppins'] text-[#616161] text-sm font-bold md:text-lg">Email (Acadêmico)</label>
-                            <Input name="email" className="font-['Poppins'] placeholder-[#808080] block bg-[#C2C2C2] rounded-xl w-80 h-14 px-5 text-sm md:h-20 md:text-lg" type="email" placeholder="seu.nome@ufcg.edu.br" required/>
+                            <Input name="email" className="font-['Poppins'] placeholder-[#808080] block bg-[#C2C2C2] rounded-xl w-80 h-14 px-5 text-sm md:h-14 md:text-base" type="email" placeholder="seu.nome@ufcg.edu.br" required/>
                             {emailErr && <p className="text-red-500">Email inválido *</p>}
                         </div>
 
-                        <div className="flex flex-col gap-3">
+                        <div className="flex flex-col gap-2">
                             <label className="font-['Poppins'] text-[#616161] text-sm font-bold md:text-lg">Telefone</label>
-                            <Input name="telephone" className="font-['Poppins'] placeholder-[#808080] block bg-[#C2C2C2] rounded-xl w-80 h-14 px-5 text-sm md:h-20 md:text-lg"  type="tel" placeholder="(83)999999999" required/>
+                            <Input name="telephone" className="font-['Poppins'] placeholder-[#808080] block bg-[#C2C2C2] rounded-xl w-80 h-14 px-5 text-sm md:h-14 md:text-base"  type="tel" placeholder="(83)999999999" required/>
                         </div>
-                        <div className="flex flex-col gap-3">
+                        <div className="flex flex-col gap-2">
                             <label className="font-['Poppins'] text-[#616161] text-sm font-bold md:text-lg">Matricula</label>
-                            <Input name="matricula" className="font-['Poppins'] placeholder-[#808080] block bg-[#C2C2C2] rounded-xl w-80 h-14 px-5 text-sm md:h-20 md:text-lg" type="text" placeholder="999999999" required/>
+                            <Input name="matricula" className="font-['Poppins'] placeholder-[#808080] block bg-[#C2C2C2] rounded-xl w-80 h-14 px-5 text-sm md:h-14 md:text-base" type="text" placeholder="999999999" required/>
                             {matriculaErr && <p className="text-red-500">Matricula invalida *</p>}
                         </div>
-                        <div className="flex flex-col gap-3">
+                        <div className="flex flex-col gap-2">
                             <label className="font-['Poppins'] text-[#616161] text-sm font-bold md:text-lg">Senha:</label>
-                            <Input name="password" className="font-['Poppins'] placeholder-[#808080] block bg-[#C2C2C2] rounded-xl w-80 h-14 px-5 text-sm md:h-20 md:text-lg" type="password" placeholder="*********" required/>
+                            <Input name="password" className="font-['Poppins'] placeholder-[#808080] block bg-[#C2C2C2] rounded-xl w-80 h-14 px-5 text-sm md:h-14 md:text-base" type="password" placeholder="*********" required/>
                             {passwordErr && <p className="text-red-500">Senha muito curta *</p>}
                         </div>
-                        <div className="flex flex-col gap-3">
+                        <div className="flex flex-col gap-2">
                             <label className="font-['Poppins'] text-[#616161] text-sm font-bold md:text-lg">Confirme sua senha:</label>
-                            <Input name="confirmPassowrd" className="font-['Poppins'] placeholder-[#808080] block bg-[#C2C2C2] rounded-xl w-80 h-11 px-5 text-sm md:h-20 md:text-lg" type="password"  placeholder="*********" required/>
+                            <Input name="confirmPassowrd" className="font-['Poppins'] placeholder-[#808080] block bg-[#C2C2C2] rounded-xl w-80 h-11 px-5 text-sm md:h-14 md:text-base" type="password"  placeholder="*********" required/>
                         </div>
                         <button className="bg-[#FFB400] rounded-lg h-12 font-bold text-white text-xl md:h-16 md:text-3xl" type="submit">Cadastrar</button>
                         <div>
-                            <p>Ao se inscrever, você concorda com nossos</p>
-                            <p>Termos de Uso e com a Política de Privacidade.</p>
+                            <p className="text-xs md:text-sm flex justify-center">Ao se inscrever, você concorda com nossos</p>
+                            <p className="text-xs md:text-sm flex justify-center">Termos de Uso e com a Política de Privacidade.</p>
                         </div>
                     </div>
                 </Form>
