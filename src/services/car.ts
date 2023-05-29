bcebf183d275f9c9eb2c67a331f0ac1f7313d75e
Vinicius Xavier <vinicius.muniz@ccc.ufcg.edu.br>
2023-05-29 19:54:14 -0300

    wip: integration with car endpoint

--- src/services/car.ts
+++ src/services/car.ts
@@ -19,8 +19,49 @@ type Token = {
 export async function createCar(car: Car) {
     const token = localStorage.getItem("bigu-token")
   try{
-    return api.post('/api/v1/cars', car);
-  }catch (error: any){
+    const response = await api.post('/api/v1/cars', car, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    return response.data as Car;
+
+    }catch (error: any){
     handleError(error);
   }
+}
+
+export async function getUserCars(): Promise<Car[]> {
+    const token = localStorage.getItem('bigu-token');
+
+    try {
+        const response = await api.get('/api/v1/cars', {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        return response.data as Car[];
+    } catch (error: any) {
+        handleError(error);
+        throw error;
+    }
+}
+
+export async function deleteCar(id: number) {
+    const token = localStorage.getItem('bigu-token');
+
+    try {
+        const response = await api.delete(`/api/v1/cars/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        return response.data;
+    } catch (error: any) {
+        handleError(error);
+        throw error;
+    }
 }
