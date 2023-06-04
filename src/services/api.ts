9edb8aa9b317f1c1b8e1b0d5129b801cf7d4fb84
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2023-06-04 17:28:31 -0300

    feat: intercept response

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { getCookie } from "cookies-next";
-import { parseCookies } from "nookies";
+import { parseCookies, destroyCookie } from "nookies";
 
 export const api = axios.create({
   baseURL: 'http://localhost:8080',
@@ -15,6 +15,28 @@ export const api = axios.create({
 //   token = token ? getCookie("token") : null;
 // }
 
+api.interceptors.response.use(
+  (response) => {
+    // Sucesso na resposta - retornar a resposta sem fazer alterações
+    return response;
+  },
+  (error) => {
+    // console.log(JSON.stringify(error, null, 2))
+    if (error.response.status === 500 || error.response.status === 404 || error.response.status === 400 || error.response.status === 403) {
+      destroyCookie(null, 'nextauth.token');
+    // Erro na resposta - manipular o erro ou lançar um novo erro
+    // Aqui você pode fazer qualquer manipulação desejada no erro de resposta
+    }
+    // Por exemplo, você pode adicionar uma propriedade personalizada ao erro
+    error.customProperty = 'Custom Value';
+
+    // Ou você pode lançar um novo erro com uma mensagem personalizada
+    throw new Error('Ocorreu um erro na resposta.');
+
+    // Se você deseja retornar uma resposta de erro modificada, você pode fazer algo como:
+    // return { ...error.response, customProperty: 'Custom Value' };
+  }
+);
 
 api.interceptors.request.use( config => {
   const { 'nextauth.token': token } = parseCookies();
@@ -22,6 +44,9 @@ api.interceptors.request.use( config => {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
+  // console.log(JSON.stringify(config, null, 2))
   return config
 })
+
 
