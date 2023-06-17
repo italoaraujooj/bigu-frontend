cefe86a4e0f16563a8caf8aef4e3f116dfb811a6
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2023-06-17 13:16:50 -0300

    feat: up

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -4,12 +4,12 @@ import CarSecondary from "../../assets/car-secondary.png";
 import clsx from "clsx";
 import Image from "next/image";
 import Text from "../text";
-import SportCar from "../../assets/sport-car.png"
+import SportCar from "../../assets/sport-car.png";
 import Modal from "../modal";
 import Input from "../input/input";
 
 type Props = {
-  profile?: boolean
+  profile?: boolean;
 };
 
 interface CarsGarageProps {
@@ -65,7 +65,8 @@ const Carousel = (props: Props) => {
         onMouseLeave={() => goToIndex(selectedCar)}
         className={`w-full flex items-center overflow-hidden space-x-12`}
       >
-        {!!items && !props.profile &&
+        {!!items &&
+          !props.profile &&
           items?.map(({ id, model, car }, index) => (
             <div
               key={id}
@@ -77,8 +78,18 @@ const Carousel = (props: Props) => {
               )}
             >
               <div className="w-full h-20 bg-extralight px-6 rounded-lg flex items-center justify-between ">
-                <Image className="w-10 h-10 lg:w-12 lg:h-12" src={car} alt="car" />
-                <Text label={model} size="md" color="gray" weight="medium" className="text-sm lg:text-md" />
+                <Image
+                  className="w-10 h-10 lg:w-12 lg:h-12"
+                  src={car}
+                  alt="car"
+                />
+                <Text
+                  label={model}
+                  size="md"
+                  color="gray"
+                  weight="medium"
+                  className="text-sm lg:text-md"
+                />
                 <button className="h-12 font-family bg-green rounded-lg text-white font-semibold text-sm lg:text-base px-4 lg:px-6 flex items-center justify-between gap-3 lg:gap-6 uppercase tracking-tight hover:bg-hover-green">
                   <div
                     className="relative w-6 h-6 rounded-full bg-white flex items-center justify-center"
@@ -109,21 +120,26 @@ const Carousel = (props: Props) => {
               )}
             >
               <div className="w-full h-44 bg-extralight px-6 rounded-lg flex items-start justify-between md:h-48 pt-6 pl-8">
-                <Image className="w-12 h-12" src={SportCar} alt="car"/>
+                <Image className="w-12 h-12" src={SportCar} alt="car" />
                 <div className="flex flex-col items-center text-gray font-medium text-xs md:text-lg">
-                  <div className="bg-light-blue h-9 p-3 flex items-center rounded-md font-bold text-white text-sm md:text-lg md:p-4">Modelo</div>
+                  <div className="bg-light-blue h-9 p-3 flex items-center rounded-md font-bold text-white text-sm md:text-lg md:p-4">
+                    Modelo
+                  </div>
                   <p>Modelo</p>
                 </div>
                 <div className="flex flex-col items-center text-gray font-medium text-xs md:text-lg">
-                  <div className="bg-light-yellow h-9 p-3 flex items-center rounded-md font-bold text-white text-sm md:text-lg md:p-4">Capacidade</div>
+                  <div className="bg-light-yellow h-9 p-3 flex items-center rounded-md font-bold text-white text-sm md:text-lg md:p-4">
+                    Capacidade
+                  </div>
                   <p>Capacidade</p>
                 </div>
                 <div className="flex flex-col items-center text-gray font-medium text-xs md:text-lg">
-                  <div className="bg-orange h-9 p-3 flex items-center rounded-md font-bold text-white text-sm md:text-lg md:p-4">Placa</div>
+                  <div className="bg-orange h-9 p-3 flex items-center rounded-md font-bold text-white text-sm md:text-lg md:p-4">
+                    Placa
+                  </div>
                   <p>Placa</p>
                 </div>
               </div>
-              
             </div>
           ))}
       </div>
