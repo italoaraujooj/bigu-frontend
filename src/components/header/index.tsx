443ba1b3be0f01b7cee18cb914203034fb235987
Matheus <matalves12345@gmail.com>
2024-09-24 17:32:32 -0300

    ajustando responsividade dos componentes

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -74,7 +74,7 @@ export default function Header(props: Props) {
         </button>
         <button onClick={props.handleOpenRequests} className="group transition-all duration-300 ease-in-out">
           <Text
-            label="Solicitações de carona"
+            label="Solicitações"
             className="py-2 text-gray text-base hover:text-[#a8a29e] uppercase font-medium bg-left-bottom bg-gradient-to-r from-amber-400 to-amber-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out underline-offset-8"
             color="gray"
             size="base"
@@ -107,6 +107,8 @@ export default function Header(props: Props) {
       <Drawer
         drawerIsOpen={drawerIsOpen}
         toggleDrawer={toggleDrawer}
+        handleOpenRequests={props.handleOpenRequests}
+        handleOpenRides={props.handleOpenRides}
         handleNavigateToOfferRide={handleNavigateToOfferRide}
         handleLogout={handleLogout}
       />
