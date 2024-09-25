c5de25865f5e3bdc7d8ab87a7211fc22038a67f7
Paulo Hernane <paulohernane10@gmail.com>
2024-09-25 10:48:04 +0000

    Refactor API service to update base URL and headers

--- src/components/offerRide/form/index.tsx
+++ src/components/offerRide/form/index.tsx
@@ -1,34 +1,34 @@
-import React, {useContext, useEffect, useRef, useState} from "react";
-import { Form } from "@unform/web";
-import { FormHandles, SubmitHandler } from "@unform/core";
 import {
-  Text,
-  Input,
   Button,
-  Dropdown,
   Carousel,
-  TextArea,
-  NumericField,
   Checkbox,
+  Dropdown,
+  Input,
+  NumericField,
+  Text,
+  TextArea,
 } from "@/components";
-import { Address, OfferRideFormState } from "@/utils/types";
-import { checkboxesOptions, fieldsLastRow } from "@/utils/offer-ride-constants";
-import { formatDateTime, moneyMask } from "@/utils/masks";
+import { AuthContext } from "@/context/AuthContext";
 import useFields from "@/hooks/useFields";
 import { fetchUserAddresses } from "@/services/address";
-import {AuthContext} from "@/context/AuthContext";
-import { toast } from "react-toastify";
 import { createRide, editRide, getRide } from "@/services/ride";
-import Router, { useRouter } from "next/router";
+import { formatDateTime, moneyMask } from "@/utils/masks";
+import { checkboxesOptions, fieldsLastRow } from "@/utils/offer-ride-constants";
+import { Address, OfferRideFormState } from "@/utils/types";
+import { FormHandles, SubmitHandler } from "@unform/core";
+import { Form } from "@unform/web";
+import Router from "next/router";
+import { useContext, useEffect, useRef, useState } from "react";
+import { toast } from "react-toastify";
 
 interface FormatAddress {
-  value: string
-  label: string
+  value: string;
+  label: string;
 }
 
 type Props = {
   rideId?: string;
-}
+};
 
 function OfferRideForm(props: Props) {
   const { rideId } = props;
@@ -37,84 +37,91 @@ function OfferRideForm(props: Props) {
   const { createFields } = useFields();
 
   const { user } = useContext(AuthContext);
-  
+
   const formRef = useRef<FormHandles>(null);
   const [checkboxes, setCheckboxes] = useState(checkboxesOptions);
   const [userAddresses, setUserAddresses] = useState<FormatAddress[]>([]);
-  const [ufcgAddressesSelected, setUfcgAddressesSelected] = useState<FormatAddress>({} as FormatAddress);
-  const [userAddressesSelected, setUserAddressesSelected] = useState<FormatAddress>({} as FormatAddress);
+  const [ufcgAddressesSelected, setUfcgAddressesSelected] =
+    useState<FormatAddress>({} as FormatAddress);
+  const [userAddressesSelected, setUserAddressesSelected] =
+    useState<FormatAddress>({} as FormatAddress);
   const [vacancies, setVacancies] = useState(0);
   const [onlyWomanChecked, setOnlyWomanChecked] = useState(true);
   const [selectedCar, setSelectedCar] = useState("");
   const [description, setDescription] = useState("");
   const [scheduledTime, setScheduledTime] = useState("");
 
-// Atualize o estado quando o valor de ride for carregado
-useEffect(() => {
-  if (ride?.scheduledTime) {
-    setScheduledTime(ride.scheduledTime);
-  }
-}, [ride?.scheduledTime]);
+  // Atualize o estado quando o valor de ride for carregado
+  useEffect(() => {
+    if (ride?.scheduledTime) {
+      setScheduledTime(ride.scheduledTime);
+    }
+  }, [ride?.scheduledTime]);
 
-// Renderize o campo, seja com o valor ou vazio
-createFields(fieldsLastRow(scheduledTime), "w-full flex items-end gap-4 space-y-2")
-
+  // Renderize o campo, seja com o valor ou vazio
+  createFields(
+    fieldsLastRow(scheduledTime),
+    "w-full flex items-end gap-4 space-y-2"
+  );
 
   const ufcgAddresses = [
     {
       value: "66e093bfe2323b4802da45c3",
-      label:"Entrada principal"
+      label: "Entrada principal",
     },
     {
       value: "66e09414e2323b4802da45c5",
-      label:"Entrada CEEI"
+      label: "Entrada CEEI",
     },
     {
       value: "66e09431e2323b4802da45c7",
-      label:"Entrada Humanas"
+      label: "Entrada Humanas",
     },
     {
       value: "66e09466e2323b4802da45c9",
-      label:"Entrada CCT"
-    }
-  ]
+      label: "Entrada CCT",
+    },
+  ];
 
   useEffect(() => {
     const loadData = async () => {
-      const responseAddress = await fetchUserAddresses()
-      if(responseAddress?.data){
-        const addressesFormated = responseAddress.data.userAddress.map((address: Address) => ({
-          label: address.nome,
-          value: address.addressId,
-        }));
+      const responseAddress = await fetchUserAddresses();
+      if (responseAddress?.data) {
+        const addressesFormated = responseAddress.data.userAddress.map(
+          (address: Address) => ({
+            label: address.nome,
+            value: address.addressId,
+          })
+        );
         setUserAddresses(addressesFormated);
       }
-    }
+    };
     loadData();
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (rideId) {
       const loadData = async () => {
-        const responseRide = await getRide(rideId)
-        if(responseRide?.data){
+        const responseRide = await getRide(rideId);
+        if (responseRide?.data) {
           setRide(responseRide?.data?.ride);
           setDescription(responseRide.data.ride.description);
           setScheduledTime(responseRide.data.ride.scheduledTime);
-          console.log(scheduledTime)
+          console.log(scheduledTime);
           if (!responseRide.data.ride.goingToCollege) {
-            handleCheckboxChange(2)
+            handleCheckboxChange(2);
           }
           setVacancies(responseRide.data.ride.numSeats - 1);
-          const startAddressFormated = 
-          {
+          const startAddressFormated = {
             label: responseRide.data.ride.startAddress.nome,
             value: responseRide.data.ride.startAddress.addressId,
           };
           const destinationAddressFormated = ufcgAddresses.find(
-            (address) => address.value === responseRide?.data?.ride?.destinationAddress?.addressId
+            (address) =>
+              address.value ===
+              responseRide?.data?.ride?.destinationAddress?.addressId
           );
-          if(ride?.goingToCollege == true) {
+          if (ride?.goingToCollege == true) {
             setUserAddressesSelected(startAddressFormated);
             setUfcgAddressesSelected(destinationAddressFormated);
           } else {
@@ -122,10 +129,10 @@ createFields(fieldsLastRow(scheduledTime), "w-full flex items-end gap-4 space-y-
             setUfcgAddressesSelected(startAddressFormated);
           }
         }
-      }
+      };
       loadData();
     }
-  }, [rideId])
+  }, [rideId]);
 
   const handleCheckboxChange = (checkboxId: number) => {
     const updatedCheckboxes = checkboxes.map((checkbox) => {
@@ -143,15 +150,21 @@ createFields(fieldsLastRow(scheduledTime), "w-full flex items-end gap-4 space-y-
     const checkboxSelected = checkboxes[0];
     const { date, hours, estimated_value } = data;
     const { checked, value } = checkboxSelected!;
-    const startAddress = checked ? userAddressesSelected?.value : ufcgAddressesSelected?.value;
-    const destinationAddress = checked ? ufcgAddressesSelected?.value : userAddressesSelected?.value;
+    const startAddress = checked
+      ? userAddressesSelected?.value
+      : ufcgAddressesSelected?.value;
+    const destinationAddress = checked
+      ? ufcgAddressesSelected?.value
+      : userAddressesSelected?.value;
 
     const dateTime = formatDateTime(date, hours);
     const numSeats = vacancies + 1;
-    const price = Number(String(estimated_value).split(" ")[1]?.replace(",","."))
+    const price = Number(
+      String(estimated_value).split(" ")[1]?.replace(",", ".")
+    );
     const toWomen = user?.sex === "F" ? onlyWomanChecked : false;
     const carId = selectedCar;
-    console.log(description)
+    console.log(description);
     const body = {
       driver: user?.userId,
       startAddress: startAddress,
@@ -164,26 +177,26 @@ createFields(fieldsLastRow(scheduledTime), "w-full flex items-end gap-4 space-y-
       description: description,
       toWomen: toWomen,
     };
-    console.log(body)
+    console.log(body);
     if (rideId) {
-      try{
+      try {
         const response = await editRide(rideId, body);
-        if(response?.status == 200){
-          Router.push("/dashboard")
-          toast.success("A carona foi editada com sucesso")
+        if (response?.status == 200) {
+          Router.push("/dashboard");
+          toast.success("A carona foi editada com sucesso");
         }
-      }catch(err: any){
-        toast.error(err.message)
+      } catch (err: any) {
+        toast.error(err.message);
       }
     } else {
-      try{
+      try {
         const response = await createRide(body);
-        if(response?.status == 201){
-          Router.push("/dashboard")
-          toast.success("A carona foi criada com sucesso")
+        if (response?.status == 201) {
+          Router.push("/dashboard");
+          toast.success("A carona foi criada com sucesso");
         }
-      }catch(err: any){
-        toast.error(err.message)
+      } catch (err: any) {
+        toast.error(err.message);
       }
     }
   };
@@ -210,10 +223,14 @@ createFields(fieldsLastRow(scheduledTime), "w-full flex items-end gap-4 space-y-
           />
         </section>
         <Dropdown
-        key={1}
+          key={1}
           label="Local de Origem"
           options={checkboxes[0].checked ? userAddresses : ufcgAddresses}
-          selectedOption={checkboxes[0].checked ? userAddressesSelected : ufcgAddressesSelected}
+          selectedOption={
+            checkboxes[0].checked
+              ? userAddressesSelected
+              : ufcgAddressesSelected
+          }
           onSelectOption={(selectedOption) =>
             checkboxes[0].checked
               ? setUserAddressesSelected(selectedOption)
@@ -221,17 +238,24 @@ createFields(fieldsLastRow(scheduledTime), "w-full flex items-end gap-4 space-y-
           }
         />
         <Dropdown
-        key={2}
+          key={2}
           label="Local de Destino"
           options={checkboxes[0].checked ? ufcgAddresses : userAddresses}
-          selectedOption={checkboxes[0].checked ? ufcgAddressesSelected : userAddressesSelected}
+          selectedOption={
+            checkboxes[0].checked
+              ? ufcgAddressesSelected
+              : userAddressesSelected
+          }
           onSelectOption={(selectedOption) =>
             checkboxes[0].checked
               ? setUfcgAddressesSelected(selectedOption)
               : setUserAddressesSelected(selectedOption)
           }
         />
-        {createFields(fieldsLastRow(scheduledTime), "w-full flex items-end gap-4 space-y-2")}
+        {createFields(
+          fieldsLastRow(scheduledTime),
+          "w-full flex items-end gap-4 space-y-2"
+        )}
         <div className="flex gap-4 items-center space-y-2">
           <div className="w-1/2 lg:w-3/4 flex flex-col ">
             <NumericField vacancies={vacancies} setVacancies={setVacancies} />
@@ -250,14 +274,16 @@ createFields(fieldsLastRow(scheduledTime), "w-full flex items-end gap-4 space-y-
             />
           </div>
         </div>
-        { user?.sex === "F" && <div>
-          <Checkbox
-            label="oferecer carona apenas para as mulheres"
-            checked={onlyWomanChecked}
-            onChange={() => setOnlyWomanChecked((prev) => !prev)}
-            className="my-4 mb-6"
-          />
-        </div> }
+        {user?.sex === "F" && (
+          <div>
+            <Checkbox
+              label="oferecer carona apenas para as mulheres"
+              checked={onlyWomanChecked}
+              onChange={() => setOnlyWomanChecked((prev) => !prev)}
+              className="my-4 mb-6"
+            />
+          </div>
+        )}
       </div>
       <div className="w-full lg:w-1/2 space-y-7">
         <div>
@@ -267,31 +293,18 @@ createFields(fieldsLastRow(scheduledTime), "w-full flex items-end gap-4 space-y-
             className="mb-4"
             weight="bold"
           />
-          <Carousel add={() => {}} setCarSelected={setSelectedCar}/>
+          <Carousel add={() => {}} setCarSelected={setSelectedCar} />
         </div>
 
         <TextArea
           label="Detalhe sua carona"
           placeholder="Detalhe um pouco da sua carona..."
           onChange={(e) => setDescription(e.target.value)}
-          value={description} // Use value instead of defaultValue
+          value={description}
         />
 
-
-        {rideId ? (
-          <section className="w-full flex justify-center gap-8">
-          <Button
-            type="button"
-            label="Editar"
-            size="base"
-            className="uppercase font-semibold px-3 lg:px-6"
-            color="green"
-            type="submit"
-          />
-        </section>
-        ) : (
-          <section className="w-full flex justify-end gap-8">
-          <Button
+        <section className="w-full flex justify-center gap-8">
+          {/* <Button
             type="button"
             label="salvar rascunho"
             size="base"
@@ -306,7 +319,6 @@ createFields(fieldsLastRow(scheduledTime), "w-full flex items-end gap-4 space-y-
             type="submit"
           />
         </section>
-        )}
       </div>
     </Form>
   );
