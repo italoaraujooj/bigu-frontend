3a6771ccb7b7df6ff3e81e78634cab89c4ee016f
Paulo Hernane <paulohernane10@gmail.com>
2024-09-25 00:54:19 +0000

    Refactor next.config.js to update @phosphor-icons/react imports and modularize imports

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  modularizeImports: {
-    "@phosphor-icons/react": {
-      transform: '@phosphor-icons/react/dist/ssr/{{member}}',
-    },
-  },
   env: {
     API_URL: process.env.API_URL,
   },
